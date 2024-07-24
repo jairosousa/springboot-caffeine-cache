97b68fc21e22adfb32712f5f470adc21c2a21ea3
Jairo Nascimento <jaironsousa@gmail.com>
2024-07-24 10:19:11 -0300

    Criação do projeto e mysql container

---
+++ src/main/java/com/jnsdevs/springcaffeinecache/SpringcaffeinecacheApplication.java
@@ -0,0 +1,13 @@
+package com.jnsdevs.springcaffeinecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcaffeinecacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcaffeinecacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jnsdevs/springcaffeinecache/SpringcaffeinecacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnsdevs.springcaffeinecache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcaffeinecacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
